c4f4fdbf507cba488ec032049ada356002ee8cb0
Vladislav Mamonov <VMwork0123@gmail.com>
2019-04-25 19:32:14 +0700

    Upgrade welcome function

--- src/IrregularVerbs.c
+++ src/IrregularVerbs.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 void welcome()
 {
@@ -12,6 +13,9 @@ void welcome()
 	printf("\n");
 
 	printf("In the process, the initial form of the verb will be provided. You must write the other two.\n");
+	printf("\n");
+	printf("Get ready...\n");
+	sleep(5);
 }
 
 int input(struct IrregularVerbs* infinitive, struct IrregularVerbs* PastSimple, struct IrregularVerbs* PastParticiple)
@@ -22,7 +26,7 @@ int input(struct IrregularVerbs* infinitive, struct IrregularVerbs* PastSimple, 
 	scanf("%s\n", &PastSimple->user[n]);
 	
 	for (int i = 0; i < n; i++)
-		PastSimple->user[i] = tolower(PastSimple->user[i]);
+		PastSimple->user[i] = tolower(PastSimple->user[i]);	//Перевод введённых данных в нижний регистр
 
 	printf("Enter the third form: ");
 	scanf("%s\n", &PastParticiple->user[n]);
